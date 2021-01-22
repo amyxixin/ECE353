f464bad7a7657f8854d74f37ce5ddb2b6d74d1f1
Amy Xin <linjing.xin@mail.utoronto.ca>
2021-01-21 20:43:45 -0500

    update words.c and complete fact.c

--- warmup/fact.c
+++ warmup/fact.c
@@ -1,18 +1,20 @@
 #include "common.h"
 #include <stdio.h>
 int factorial(int n){
-	if (n==1){
+	if (n==0){
 		return 1;
 	}else{
-		return n*(n-1);
+		return n*factorial(n-1);
 	}
 }
 int
 main(int argc, char *argv[])
 {
 	if (argc >=1){
-		int num = (int)(argv[1]);
-		printf("%d",factorial(num));
+		int num = atoi(argv[1]);
+		if (num>=0){
+			printf("%d\n",factorial(num));
+		}
 	}
 	return 0;
 }
